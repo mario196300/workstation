23979bc679ef24c19200c4e8a373f1623ad98491
JM <JM@centos-6>
2017-06-12 11:14:12 +0200

    Cambio de version

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,7 +1,14 @@
 package 'tree'
 
 file '/etc/motd' do
-   content 'Property of JM'
+   content "Property of JM
+
+   IPADDRESS: #{node['ipaddress']}
+   HOSTNAME : #{node['hostname']}
+   MEMORY   : #{node['memory']['total']}
+   CPU      : #{node['cpu']['0']['mhz']}
+"
+   mode  '0644'
    owner 'root'
    group 'root'
 end
