95e9fb6142e8445db54702b6c7fbf078a93c04af
JM <JM@centos-6>
2017-06-12 13:33:35 +0200

    Modificacion de templates

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,13 +1,8 @@
 package 'tree'
 
-file '/etc/motd' do
-   content "Property of JM
-
-   IPADDRESS: #{node['ipaddress']}
-   HOSTNAME : #{node['hostname']}
-   MEMORY   : #{node['memory']['total']}
-   CPU      : #{node['cpu']['0']['mhz']}
-"
+template '/etc/motd' do
+   source 'motd.erb'
+   
    mode  '0644'
    owner 'root'
    group 'root'
